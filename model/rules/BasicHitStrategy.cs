91f129edc8f7b8fffe064029faf05218353404a0
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-10-28 18:11:21 +0100

    split Hit strategies

--- model/rules/BasicHitStrategy.cs
+++ model/rules/BasicHitStrategy.cs
@@ -11,15 +11,6 @@ namespace BlackJack.model.rules
 
         public bool DoHit(model.Player a_dealer)
         {
-            IEnumerable<Card> cards = a_dealer.GetHand();
-
-            foreach (Card card in cards)
-            {
-                if (card.GetValue() == Card.Value.Ace && a_dealer.CalcScore() == g_hitLimit)
-                {
-                    return true;
-                }
-            }
             return a_dealer.CalcScore() < g_hitLimit;
         }
     }
